90e8cebc4b47974ff1f4109f8d0fa849a90c730a
Austin Chiatto <chiattoaustin@gmail.com>
2024-05-25 19:17:04 -0700

    Improved specificity of translateDate function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,35 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const translateDate = (dateString: string): string => {
-  const dateObj = new Date(dateString);
-  const now = new Date();
-  const diffInMilliseconds = dateObj.getTime() - now.getTime();
-  const diffInHours = diffInMilliseconds / (1000 * 60 * 60);
-
-  const isSameYear = now.getFullYear() === dateObj.getFullYear();
-
-  if (diffInHours < 0) {
-    return 'Past date'; // Handle past dates if needed
-  } else if (diffInHours < 24) {
-    return `Today at ${dateObj.toLocaleTimeString('en-US', {
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: true
-    })}`;
-  } else if (diffInHours < 48) {
-    return `Tomorrow at ${dateObj.toLocaleTimeString('en-US', {
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: true
-    })}`;
-  } else if (diffInHours < 24 * 365 && isSameYear) {
-    return dateObj.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
-  } else {
-    return dateObj.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
-  }
-};
-
 export const getStatusClass = (id: number) => {
   if ([6, 1, 3].includes(id)) {
     return 'bg-green-500';
@@ -42,6 +13,54 @@ export const getStatusClass = (id: number) => {
   } else if ([4, 7].includes(id)) {
     return 'bg-red-500';
   } else {
-    return 'bg-gray-500'; // Fallback color if id doesn't match any condition
+    return 'bg-gray-500';
   }
+};
+
+export const translateDate = (dateString: string): string => {
+  const dateObj = new Date(dateString);
+  const now = new Date();
+  const diffInMilliseconds = now.getTime() - dateObj.getTime();
+  const diffInHours = diffInMilliseconds / (1000 * 60 * 60);
+
+  const isSameYear = now.getFullYear() === dateObj.getFullYear();
+  const isYesterday =
+    diffInHours >= 24 && diffInHours < 48 && now.getDate() - dateObj.getDate() === 1 && isSameYear;
+
+  if (diffInHours < 0) {
+    if (Math.abs(diffInHours) < 24) {
+      return `Today at ${dateObj.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true
+      })}`;
+    }
+    if (Math.abs(diffInHours) < 48) {
+      return `Tomorrow at ${dateObj.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true
+      })}`;
+    }
+    if (Math.abs(diffInHours) < 24 * 365 && isSameYear) {
+      return dateObj.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
+    }
+    return dateObj.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
+  }
+
+  if (diffInHours < 24) {
+    return `${Math.floor(diffInHours)} hours ago`;
+  }
+  if (isYesterday) {
+    return `Yesterday at ${dateObj.toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true
+    })}`;
+  }
+  if (diffInHours < 24 * 365 && isSameYear) {
+    return dateObj.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
+  }
+
+  return dateObj.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
 };
